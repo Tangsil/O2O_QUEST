345b5e4900fcca283abbdd48f2f429c1b40ca620
tangsil <tangsil@gmail.com>
2023-02-27 12:12:08 +0100

    Initial Commit

---
+++ O2O_QUEST/O2O_QUESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  O2O_QUESTApp.swift
+//  O2O_QUEST
+//
+//  Created by Jose on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct O2O_QUESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
